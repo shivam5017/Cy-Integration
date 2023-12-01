6aca05ccf57716e712a81f08486f7306d3a456e7
shivam5017 <shivammalik962@gmail.com>
2023-12-01 21:30:04 +0530

    Integration-1 L :

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Github Integration</h1>
+     <button>Checking</button>
+    </div>
+  );
+}
+
+export default App;
