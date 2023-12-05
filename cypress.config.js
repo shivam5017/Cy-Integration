df1e810febb82cc2b320e6f00e0ae2185bab4f40
shivam5017 <shivammalik962@gmail.com>
2023-12-05 15:11:59 +0530

    url change

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-     baseUrl: 'https://example.cypress.io', 
+    //  baseUrl: 'https://example.cypress.io', 
+    baseUrl: 'http://localhost:3000/'
   },
   
 });
--- cypress/e2e/react.cy.js
+++ cypress/e2e/react.cy.js
@@ -7,7 +7,7 @@ describe('template spec', () => {
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit('/todo')
+    cy.visit('/')
     
   })
   it('displays heading', () => {
